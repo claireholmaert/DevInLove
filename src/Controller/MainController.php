1807bf2e0cfb598f99539f1096c3e69089a64d16
miliweboss <claireholmaert@gmail.com>
2023-05-06 14:15:31 +0200

    Dev In Love

---
+++ src/Controller/MainController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/main',
+    name: 'main')]
+class MainController extends AbstractController
+{
+    #[Route('/',
+        name: '_index')]
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig');
+    }
+
+    #[Route('/home',
+        name: '_home')]
+    public function home(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
---
+++ src/Entity/Message.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MessageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MessageRepository::class)]
+class Message
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $sujet = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $corps = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSujet(): ?string
+    {
+        return $this->sujet;
+    }
+
+    public function setSujet(string $sujet): self
+    {
+        $this->sujet = $sujet;
+
+        return $this;
+    }
+
+    public function getCorps(): ?string
+    {
+        return $this->corps;
+    }
+
+    public function setCorps(?string $corps): self
+    {
+        $this->corps = $corps;
+
+        return $this;
+    }
+}
